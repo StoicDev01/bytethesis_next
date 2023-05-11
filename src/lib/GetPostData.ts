4070c43c45eb04c4fa5ea92837eaeffedf156429
izkeas <izkeasyomito@gmail.com>
2023-05-11 18:02:27 -0300

    Fixed content summary display

--- src/lib/GetPostData.ts
+++ src/lib/GetPostData.ts
@@ -8,45 +8,49 @@ import matter from "gray-matter";
 import PostMetadata from "./PostMetadata";
 import { MetaPage } from "./MetaPage";
 
+import {convert} from "html-to-text"
+
 function extractSummary(content : string){
-  return content.substring(0, 400) + "...";
+  // extract text
+  const text = convert(content)
+
+  return text.substring(0, 400) + "...";
 }
 
 
 export async function getPostData(language : string, id  : string, withContent=false) : Promise<MetaPage> {
     const fullPath = path.join(`src/content/${language}`, `${id}.md`);
     const fileContents = fs.readFileSync(fullPath, 'utf8');
-  
+
     // Use gray-matter to parse the post metadata section
     const matterResult = matter(fileContents);
-    const postMetadata  : PostMetadata = (matterResult.data) as PostMetadata;
-    const postLink = `/${ language != "en" ? (language + "/") : ""}posts/${id}`;
-    const summary = extractSummary(matterResult.content);
-
+  
     // Use remark to convert markdown into HTML string
     let contentHtml;
 
-    if (withContent){
-      const md = Markdown({
-        html : true
-      });
+    const md = Markdown({
+      html : true
+    });
 
-      md.use(prism)
+    md.use(prism)
 
-      contentHtml = md.render(matterResult.content);
-    }
+    contentHtml = md.render(matterResult.content);
 
+    const postMetadata  : PostMetadata = (matterResult.data) as PostMetadata;
+    const postLink = `/${ language != "en" ? (language + "/") : ""}posts/${id}`;
+    const summary = extractSummary(contentHtml);
+
+  
     const newMetaPage :MetaPage = {
       ...postMetadata,
       summary : summary,
       link : postLink
     }
 
-    if (contentHtml){
+    if (withContent){
       newMetaPage.contentHtml = contentHtml;
     }
 
-    // Combine the data with the id and contentHtml
     return newMetaPage;
   }
   
